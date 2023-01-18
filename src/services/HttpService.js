1de1ed324c3cb4ea0c52adce0dd85d6e76228d67
Sina Ahmadi <Sinacket@gmail.com>
2023-01-19 02:42:20 +0330

    BaseURL in production fixed

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const app = axios.create({
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.NODE_ENV === "development" ? 'http://localhost:3000/api' : 'https://next-crypto-tau.vercel.app/api'
 })
 
 const http = {
